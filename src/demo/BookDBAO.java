1f7cffb39ca9a1cedecdfab5abab4c79d0a2a582
yudaliang <daliang_91@hotmail.com>
2015-10-29 18:12:17 +0800

    verson1

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,31 @@
+package demo;
+
+/**
+ * Created by Liang on 29/10/2015.
+ */
+public class BookDBAO {
+    // get book details given the bookId
+    // this method is dummy for now
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+
+
+}
